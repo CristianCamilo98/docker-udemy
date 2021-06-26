7702a939b1e7413770263b16f36c41e54da58436
Cristian Morales <cristian.morales@makingscience.com>
2021-06-26 15:28:37 +0200

    New deployment

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Aprobaras seguridad :)
+		Deja de quejarte tanto que te estoy ensenyando muchas cosas bb :) 
         </p>
         <a
           className="App-link"
